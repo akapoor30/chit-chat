67049ffeca9dae18a1b2305c70df039f50260c9d
Ansh <ansh28508@gmail.com>
2023-07-08 18:29:48 +0530

    connecting to mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
 
-app.get('/',(req,res)=> {
-    res.send("API is Running Successfully");
-});
-app.get('/api/chat',(req,res)=> {
-    res.send(chats);
-});
+connectDB();
+const app = express();
 
-app.get("/api/chat/:id", (req,res) => {
-    // console.log(req.params.id);
-    const singleChat =chats.find((c)=> c._id === req.params.id);
-    res.send(singleChat);
+app.get("/", (req, res) => {
+  res.send("API is Running Successfully");
+});
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
 });
 
-const PORT = process.env.PORT || 5000
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
-app.listen(5000,console.log(`Server Started on PORT ${PORT} `));
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT} `.yellow.bold));
